845a654971b8278b8349fbb58341c4f1428157c1
Тисин Глеб Романович <iv122s23@bwp05.csc.local>
2022-04-27 13:04:24 +0700

    make at home =)

--- lab.c
+++ lab.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 size_t encode_varint(uint32_t value, uint8_t* buf)
 {
@@ -50,8 +51,21 @@ uint32_t generate_number()
 
 int main()
 {
-	FILE *fopen(uncompressed.dat, "wb");
+	FILE *uncompressed, *compressed;
+	uint32_t buffer[1000000];
+	uint8_t buf[4];
+	uncompressed = fopen("uncompressed.dat", "wb");
 	for(int i = 0; i < 1000000; i++){
-		generate_number();
+		buffer[i] = generate_number();
 	}
+	fwrite(buffer, sizeof(uint32_t), 1000000, uncompressed);
+	fclose(uncompressed);
+	
+	compressed = fopen("compressed.dat", "wb");
+	for(int i = 0; i < 1000000; i++){
+		size_t length = encode_varint(buffer[i], buf);
+		fwrite(buf, sizeof(uint8_t), length, compressed);
+	}
+	fclose(compressed);
+	
 }
